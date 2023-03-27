235008bdc6e621ca0b62a671a2be692a90289e36
Arda Kılıç <87716329+ardaklc0@users.noreply.github.com>
2023-03-27 16:54:37 +0300

    Add files via upload

---
+++ OpenClosedPrincipleExample/OpenClosedPrincipleExample/Program.cs
@@ -0,0 +1,71 @@
+﻿double basePrice = 1235.65;
+
+Customer ARDA_KILIC = new Customer { Name = "Arda", Surname = "Kılıç", ChoosenRoom = new King() };
+Customer ASLAN_KILIC = new Customer { Name = "Aslan", Surname = "Kılıç", ChoosenRoom = new Deluxe() };
+
+RoomManager roomManager_ASLAN_KILIC = new RoomManager() { Customer = ASLAN_KILIC };
+RoomManager roomManager_ARDA_KILIC = new RoomManager() { Customer = ARDA_KILIC };
+
+double occupiedPrice_ARDA_KILIC = roomManager_ARDA_KILIC.GetCheckOutBill(basePrice, 13);
+double occupiedPrice_ASLAN_KILIC = roomManager_ASLAN_KILIC.GetCheckOutBill(basePrice, 13);
+Console.WriteLine(occupiedPrice_ARDA_KILIC);
+Console.WriteLine(occupiedPrice_ASLAN_KILIC);
+
+
+
+
+
+public interface IHotelRoom
+{
+    public double Bill(double basePrice);
+}
+
+public class Standart : IHotelRoom
+{
+    public double Bill(double basePrice)
+    {
+        return basePrice;
+    }
+}
+
+public class Deluxe : IHotelRoom
+{
+    public double Bill(double basePrice)
+    {
+        return basePrice * 1.1;
+    }
+}
+
+public class Suite : IHotelRoom
+{
+    public double Bill(double basePrice)
+    {
+        return basePrice * 1.5;
+    }
+}
+
+public class King : IHotelRoom
+{
+    public double Bill(double basePrice)
+    {
+        return basePrice * 2.5;
+    }
+}
+
+public class Customer
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public IHotelRoom ChoosenRoom { get; set; }
+}
+
+
+public class RoomManager
+{
+    public Customer Customer { get; set; }
+
+    public double GetCheckOutBill(double basePrice, int occupiedDays)
+    {
+        return Customer.ChoosenRoom.Bill(basePrice) * occupiedDays;
+    }
+}
